8c46b59ccbf1cbbabb199a0740b83502a2554555
Khalux27 <andrew.xk.do@gmail.com>
2025-11-29 11:30:42 -0800

    Add Create Account page/controller + small Login page controller polishing

---
+++ Client/src/main/java/com/propertypal/client/AccountCreateController.java
@@ -0,0 +1,208 @@
+package main.java.com.propertypal.client;
+
+import com.propertypal.client.SceneManager;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.util.Arrays;
+
+public class AccountCreateController
+{
+    @FXML
+    private VBox root;
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField confPasswordField;
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private CheckBox landlordCheckbox;
+
+    @FXML
+    private TextField streetField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private ComboBox<String> stateCombo;
+    @FXML
+    private TextField zipField;
+
+    private boolean isLandlord;
+
+    //--------------------
+    // UI Functions
+    //--------------------
+
+    @FXML
+    private void initialize()
+    {
+        // Prevents focus of elements when page loads
+        Platform.runLater(() -> root.requestFocus());
+
+        // Populate states into ComboBox
+        stateCombo.getItems().addAll(Arrays.asList(
+                "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+                "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+                "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+        ));
+
+        // Disable address fields
+        setAddressFieldState(false);
+    }
+
+    // Landlord checkbox trigger
+    @FXML
+    private void onLandlordCheckboxToggle()
+    {
+        boolean checked = landlordCheckbox.isSelected();
+        setAddressFieldState(checked);
+    }
+
+    // Submit button trigger
+    @FXML
+    private void onSubmitButtonClick()
+    {
+        validateSubmit();
+    }
+
+    // Back button trigger
+    @FXML
+    private void onBackButtonClick()
+    {
+        SceneManager.switchTo("/fxml/login.fxml");
+    }
+
+    //--------------------
+    // Helper Functions
+    //--------------------
+
+    private void setAddressFieldState(boolean enabled)
+    {
+        streetField.setDisable(!enabled);
+        cityField.setDisable(!enabled);
+        stateCombo.setDisable(!enabled);
+        zipField.setDisable(!enabled);
+    }
+
+    private void validateSubmit()
+    {
+        // Client-side validation
+
+        String first = firstNameField.getText().trim();
+        String last = lastNameField.getText().trim();
+        String email = emailField.getText().trim();
+        String phone = phoneField.getText().trim();
+        String pass = passwordField.getText().trim();
+        String confirm = confPasswordField.getText().trim();
+
+        isLandlord = landlordCheckbox.isSelected();
+
+        // Check empty fields
+        if (email.isEmpty() || first.isEmpty() || last.isEmpty() ||
+                phone.isEmpty() || pass.isEmpty() || confirm.isEmpty())
+        {
+            errorLabel.setText("All fields are required");
+            return;
+        }
+
+        // Check first and last name only contain letters
+        if (!first.matches("^[A-Za-z]+$") || !last.matches("^[A-Za-z]+$"))
+        {
+            errorLabel.setText("First and last name must contain only letters");
+            return;
+        }
+
+        // Check email is in correct format
+        if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"))
+        {
+            errorLabel.setText("Please enter a valid email address");
+            return;
+        }
+
+        // Check that passwords match
+        if (!pass.equals(confirm))
+        {
+            errorLabel.setText("Passwords do not match");
+            return;
+        }
+
+        // Check phone number only contains 10 digits
+        if (!phone.matches("^\\d{10}$"))
+        {
+            errorLabel.setText("Phone number must be 10 digits and contain no spaces/symbols");
+            return;
+        }
+
+        // Checks for landlord
+        if (isLandlord)
+        {
+            String street = streetField.getText().trim();
+            String city = cityField.getText().trim();
+            String state = stateCombo.getValue();
+            String zip = zipField.getText().trim();
+
+            // Check empty address fields
+            if (street.isEmpty() || city.isEmpty() || state == null || zip.isEmpty())
+            {
+                errorLabel.setText("All address fields are required for landlords");
+                return;
+            }
+
+            // Check zip code only contains 5 digits
+            if (!zip.matches("^\\d{5}$"))
+            {
+                errorLabel.setText("ZIP code must be 5 digits");
+                return;
+            }
+        }
+
+        // Client-side validation passed
+
+        // TODO Backend logic here (including password hashing)
+
+        showSuccessDialog();
+
+        if (isLandlord)
+        {
+            SceneManager.switchTo("/fxml/LL_main.fxml");
+        }
+        else
+        {
+            SceneManager.switchTo("/fxml/TT_main.fxml");
+        }
+    }
+
+    private void showSuccessDialog()
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Account Created");
+        alert.setHeaderText("Success!");
+
+        if (isLandlord)
+        {
+            alert.setContentText("Your landlord account has been created successfully!");
+        }
+        else
+        {
+            alert.setContentText("Your account has been created successfully!");
+        }
+
+        alert.showAndWait();
+    }
+}
--- Client/src/main/java/com/propertypal/client/LoginController.java
+++ Client/src/main/java/com/propertypal/client/LoginController.java
@@ -1,7 +1,8 @@
 package main.java.com.propertypal.client;
 
-import com.propertypal.client.SceneManager;
 import com.propertypal.client.ClientLogic.AcctLogic;
+import com.propertypal.client.SceneManager;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -11,25 +12,30 @@ import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
+
 public class LoginController
 {
     public AcctLogic login_info = new AcctLogic();
+
     @FXML
     private VBox root;
-
     @FXML
     private Label errorLabel;
 
     @FXML
     private TextField emailField;
-
     @FXML
     private PasswordField passwordField;
+
+    //--------------------
+    // UI Functions
+    //--------------------
 
     @FXML
     private void initialize()
     {
-        Platform.runLater(() -> root.requestFocus());   // Prevents focus of elements when page loads
+        // Prevents focus of elements when page loads
+        Platform.runLater(() -> root.requestFocus());
     }
 
     // Log In button trigger
@@ -39,19 +45,25 @@ public class LoginController
         validateLogin();
     }
 
-    // TEMPORARY Create Account button trigger
+    // Create Account button trigger
     @FXML
     private void onCreateAccountButtonClick()
     {
-        errorLabel.setText("REDIRECT: CREATE ACCOUNT PAGE");
+        SceneManager.switchTo("/fxml/accountCreate.fxml");
     }
+
+    //--------------------
+    // Helper Functions
+    //--------------------
 
     private void validateLogin()
     {
+        boolean loginSuccess = false;
+
+        // Client-side validation
+
         String email = emailField.getText().trim();
         String password = passwordField.getText().trim();
-        boolean loginSuccess = false;
-        ;
 
         // Check empty fields
         if (email.isEmpty() || password.isEmpty())
@@ -60,14 +72,15 @@ public class LoginController
             return;
         }
 
-        // Email validation (simple pattern)
+        // Check email is in correct format
         if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"))
         {
             errorLabel.setText("Please enter a valid email address");
             return;
         }
 
-        // Valid email + non-empty password
+        // Client-side validation passed
+
         try
         {
             //TODO need to direct tenant or landlord to corresponding landing pages
